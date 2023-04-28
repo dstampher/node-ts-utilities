eaa013ca447fb08ca3f6cffd4aab77f95bcab1cf
David Stampher <davidstampher@gmail.com>
2023-04-28 14:35:12 -0700

    renamed netUtils to httpUtils

---
+++ __tests__/integration/httpUtils.tests.ts
@@ -0,0 +1,12 @@
+import { testProxy } from "../../src/httpUtils";
+import axios from "axios";
+test("Test a valid proxy", async () => {
+  let proxy = "http://localhost:8888";
+  let result = await testProxy(
+    axios.create(),
+    proxy,
+    "http://ip-api.com/json",
+    "Santa Maria"
+  );
+  expect(result).toBe(true);
+});
